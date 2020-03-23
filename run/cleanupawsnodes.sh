dcd63337a72fd5e4015988e04cd6166aba81cba9
nnzhaocs <znannan1@vt.edu>
2020-03-23 21:09:34 +0000

    update

--- run/cleanupawsnodes.sh
+++ run/cleanupawsnodes.sh
@@ -1,5 +1,7 @@
 
-pssh -h awsnodes.lst -x "-o StrictHostKeyChecking=no -i /home/nannan/dockerswarm.pem" -l ubuntu -i 'sudo docker stop -f $(sudo docker ps -a -q)'
+pssh -h awsnodes.lst -x "-o StrictHostKeyChecking=no -i /home/nannan/dockerswarm.pem" -l ubuntu -i 'sudo docker stop $(sudo docker ps -a -q)'
+
+pssh -h awsnodes.lst -x "-o StrictHostKeyChecking=no -i /home/nannan/dockerswarm.pem" -l ubuntu -i 'sudo docker rm -f $(sudo docker ps -a -q)'
 
 pssh -h awsnodes.lst -x "-o StrictHostKeyChecking=no -i /home/nannan/dockerswarm.pem" -l ubuntu -i 'sudo docker rmi -f $(sudo docker images -a -q)'
 
